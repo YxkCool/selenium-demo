7e7099b0d76436533da0196de02bbcf198e59f74
Kayenee <zhaomosh@163.com>
2020-11-14 14:05:15 +0800

    添加成员

--- src/test/java/com/test/WeChatLogin.java
+++ src/test/java/com/test/WeChatLogin.java
@@ -4,7 +4,11 @@ package com.test;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,11 +25,13 @@ import java.util.concurrent.TimeUnit;
  * @Date: 2020/11/14 10:05
  * @since 1.0
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class WeChatLogin {
+    private  WebDriver driver;
 
-    @Test
+    @BeforeAll
     void wechatLogin() throws IOException, InterruptedException {
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx");
         Thread.sleep(15000);
@@ -35,9 +41,9 @@ public class WeChatLogin {
     }
 
 
-    @Test
+    @BeforeEach
     void wechatLogined() throws IOException, InterruptedException {
-        WebDriver driver = new ChromeDriver();
+
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx");
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
@@ -48,6 +54,16 @@ public class WeChatLogin {
             driver.manage().addCookie(new Cookie(n.get("name").toString(), n.get("value").toString()));
         });
         driver.navigate().refresh();
+    }
+
+    @Test
+    void addUser() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@node-type='addmember']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("username")).sendKeys("阿佳");
+        driver.findElement(By.id("memberAdd_acctid")).sendKeys("KAY");
+        driver.findElement(By.xpath("//*[@class='qui_btn ww_btn js_btn_save']")).click();
+
     }
 
 
